4a841710e36903bbc737e68219878c9deeffd24e
KurskijDenis <kurskij.denis@gmail.com>
2017-10-25 16:58:05 +0700

    add new files2

---
+++ TestApplication/newClass2.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "newClass2.h"
+
+
+newClass2::newClass2()
+{
+}
+
+
+newClass2::~newClass2()
+{
+}
---
+++ TestApplication/newClass2.h
@@ -0,0 +1,8 @@
+#pragma once
+class newClass2
+{
+public:
+	newClass2();
+	~newClass2();
+};
+
